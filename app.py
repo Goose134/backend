95d390be0c582d1c687967b97592f0852327557b
Goose134 <fdragon2014@gmail.com>
2026-09-23 15:09:19 +0300

    Backend version 1.0

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.post("/save")
+def save_text():
+    data = request.get_json()
+
+    text = data.get("text", "")
+
+    with open("data.txt", "w", encoding="utf-8") as file:
+        file.write(text)
+
+    return jsonify({
+        "message": "Данные успешно сохранены"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
